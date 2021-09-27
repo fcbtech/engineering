c77e4d25cc43dd1bfdacb9942d59ee978715c958
Sanket Patel <sanketpatel@Sankets-MacBook-Air.local>
2021-09-27 09:52:25 +0530

    807e072

---
+++ _nuxt/static/1632716535/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fengineering\u002F_nuxt\u002Fstatic\u002F1632716535",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fengineering\u002F",assetsPath:"\u002Fengineering\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"519f3406"}}}}(null));
